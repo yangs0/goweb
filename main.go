74566786379ffb88888eba97d930c028758feaef
shaoxia <553974235@qq.com>
2021-08-17 22:02:39 +0800

    first page

--- main.go
+++ main.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func handelFunc(w http.ResponseWriter, r *http.Request)  {
-
+func defaultHandle(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
 	if r.URL.Path == "/" {
-		_, _ = fmt.Fprint(w, "<h1>根路径</h1>"+r.URL.Path)
-	}else if r.URL.Path == "/about" {
-		_, _ = fmt.Fprint(w, "<h2>about 路径</h2>"+r.URL.Path)
+		fmt.Fprint(w, "<h1>this first page.</h1>")
 	}else {
-		_, _ = fmt.Fprint(w, "其他路径")
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "找不到链接")
 	}
 }
+
+func aboutHandle(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+
+	fmt.Fprint(w, "<h2>This is a about link. </h2>")
+}
 func main() {
-	http.HandleFunc("/", handelFunc)
-	_ = http.ListenAndServe(":80", nil)
+
+	http.HandleFunc("/", defaultHandle)
+	http.HandleFunc("/about", aboutHandle)
+
+	i := http.ListenAndServe(":80", nil)
+	log.Println(i)
 }
 
