67a61559024e23901b495ac1dfae773c3e27ea25
root <root@LAPTOP-UI6RF12D.localdomain>
2021-08-23 23:24:45 +0800

    创建页面

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strings"
 )
+var router = mux.NewRouter()
 
 func homeHandle(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html;charset=utf-8")
@@ -35,16 +37,77 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "创建新的文章")
+	if err:= r.ParseForm(); err != nil {
+		// 解析错误，这里应该有错误处理
+		fmt.Fprint(w,  "请提供正确的数据！")
+		return
+	}
+
+	title := r.PostForm.Get("title")
+
+	fmt.Fprintf(w, "POST PostForm: %v <br>", r.PostForm)
+	fmt.Fprintf(w, "POST Form: %v <br>", r.Form)
+	fmt.Fprintf(w, "title 的值为: %v", title)
+
+	fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
+	fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
+	fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
+	fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
 }
+
+func forceHTMLMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1. 设置标头
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		// 2. 继续处理请求
+		h.ServeHTTP(w, r)
+	})
+}
+
+func removeTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1. 除首页以外，移除所有请求路径后面的斜杆
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+
+		// 2. 将请求传递下去
+		next.ServeHTTP(w, r)
+	})
+}
+
+func articleCreateHandle(w http.ResponseWriter, r *http.Request) {
+	html := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 —— 我的技术博客</title>
+</head>
+<body>
+    <form action="%s?test=dattt" method="post">
+        <p><input type="text" name="title"></p>
+        <p><textarea name="body" cols="30" rows="10"></textarea></p>
+        <p><button type="submit">提交</button></p>
+    </form>
+</body>
+</html>
+`
+storeUrl,_ := router.Get("articles.store").URL()
+
+	fmt.Fprintf(w, html, storeUrl)
+}
+
 func main() {
-	router := mux.NewRouter()
+
 	router.HandleFunc("/", homeHandle).Name("home")
 	router.HandleFunc("/about", aboutHandle).Methods("GET").Name("about")
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandle).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles/create", articleCreateHandle).Methods("GET").Name("articles.create")
 
+
+	router.Use(forceHTMLMiddleware)
 	router.NotFoundHandler = http.HandlerFunc(noFountHandle)
 
 	homeURL, _ := router.Get("home").URL()
